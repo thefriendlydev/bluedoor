84175d0deaabb6e9801229660ddc3335ab2b11fb
Matt Miller <matt@milleronrails.com>
2014-10-26 17:40:38 -0400

    tweaking tags

--- single.php
+++ single.php
@@ -23,34 +23,11 @@
               <div class="single-content"><?php the_content(); ?></div>
               <div class="single-categories">
                 <ul>
-                  <?php
-                      $args = array(
-                    'show_option_all'    => '',
-                    'orderby'            => 'name',
-                    'order'              => 'ASC',
-                    'style'              => 'list',
-                    'show_count'         => 0,
-                    'hide_empty'         => 1,
-                    'use_desc_for_title' => 1,
-                    'child_of'           => 0,
-                    'feed'               => '',
-                    'feed_type'          => '',
-                    'feed_image'         => '',
-                    'exclude'            => '',
-                    'exclude_tree'       => '',
-                    'include'            => '',
-                    'hierarchical'       => 1,
-                    'title_li'           => __( '' ),
-                    'show_option_none'   => __( 'No categories' ),
-                    'number'             => null,
-                    'echo'               => 1,
-                    'depth'              => 0,
-                    'current_category'   => 0,
-                    'pad_counts'         => 0,
-                    'taxonomy'           => 'category',
-                    'walker'             => null
-                      );
-                      wp_list_categories( $args );
+                                    <?php
+                  foreach((get_the_category()) as $category) {
+                      echo '<li>' . $category->cat_name . ' </li>';
+                    // echo '<li>' . '<a href="' . get_category_link( $category->cat_ID) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->cat_name.'</a> </li>' ;
+                  }
                   ?>
                 </ul>
               </div>
