1af50013cab402c8bf9a517975bd278b6012a11c
Matt Miller <matt@milleronrails.com>
2014-12-20 23:48:19 -0500

    starting custom yarpp

--- yarpp-template-bluedoor.php
+++ yarpp-template-bluedoor.php
@@ -8,7 +8,18 @@ Description: A simple example YARPP template.
 <?php if (have_posts()):?>
 <ol>
   <?php while (have_posts()) : the_post(); ?>
-  <li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
+  <li>
+    <div class="combo">
+      <div class="combo-first">
+        <?php if (has_post_thumbnail()):?>
+          <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
+        <?php endif; ?>
+      </div>
+      <div class="combo-last">
+        <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+      </div>
+    </div>
+  </li>
   <?php endwhile; ?>
 </ol>
 <?php else: ?>
