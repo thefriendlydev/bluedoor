8c2778b5600f6d66795801c668cf922afa08714f
Matt Miller <matt@milleronrails.com>
2014-12-21 00:25:35 -0500

    adding plain list

--- yarpp-template-bluedoor.php
+++ yarpp-template-bluedoor.php
@@ -6,22 +6,24 @@ Description: A simple example YARPP template.
 */
 ?><h3>Related Posts</h3>
 <?php if (have_posts()):?>
-<ol>
-  <?php while (have_posts()) : the_post(); ?>
-  <li>
-    <div class="combo combo--middle">
-      <div class="combo-first">
-        <?php if( get_field('post_thumbnail') ): ?>
-          <a class="yarpp-bluedoor-thumbnail" href="<?php the_permalink() ?>" rel="bookmark"><img src="<?php the_field('post_thumbnail'); ?>" /></a>
-        <?php endif; ?>
+  <div class="yarppBluedoor">
+  <ul>
+    <?php while (have_posts()) : the_post(); ?>
+    <li>
+      <div class="combo combo--middle">
+        <div class="combo-first">
+          <?php if( get_field('post_thumbnail') ): ?>
+            <a class="yarppBluedoor-thumbnail" href="<?php the_permalink() ?>" rel="bookmark"><img src="<?php the_field('post_thumbnail'); ?>" /></a>
+          <?php endif; ?>
+        </div>
+        <div class="combo-last">
+          <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+        </div>
       </div>
-      <div class="combo-last">
-        <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
-      </div>
-    </div>
-  </li>
-  <?php endwhile; ?>
-</ol>
+    </li>
+    <?php endwhile; ?>
+  </ul>
+  </div>
 <?php else: ?>
 <p>No related posts.</p>
 <?php endif; ?>
