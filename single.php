adb7b553c98a98ea42af20ce61c8d0d53b387cd7
Matt Miller <matt@milleronrails.com>
2014-12-21 00:48:25 -0500

    adding in sidebar

--- single.php
+++ single.php
@@ -3,38 +3,45 @@
 <?php if ( have_posts() ) : ?>
   <?php while ( have_posts() ) : the_post(); ?>
     <div class="section single-header">
-      <?php if (  has_post_thumbnail() ) : ?>
-        <div class="container">
-          <div class="grid">
-            <div class="grid__item">
-              <div class="user-featured-image">
-                <?php the_post_thumbnail(); ?>
+      <div class="grid">
+        <div class="grid__item two-thirds palm--one-whole">
+          <?php if (  has_post_thumbnail() ) : ?>
+            <div class="container">
+              <div class="grid">
+                <div class="grid__item">
+                  <div class="user-featured-image">
+                    <?php the_post_thumbnail(); ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <?php endif; ?>
+          <div class="container container--narrow">
+            <div class="grid">
+              <div class="grid__item">
+                <div class="user-content">
+                  <div class="single-title"><?php the_title(); ?></div>
+                  <div class="single-date"><?php the_date('M d, Y'); ?></div>
+                  <div class="single-content"><?php the_content(); ?></div>
+                  <div class="single-categories">
+                    <ul>
+                                        <?php
+                      foreach((get_the_category()) as $category) {
+                          echo '<li>' . $category->cat_name . ' </li>';
+                        // echo '<li>' . '<a href="' . get_category_link( $category->cat_ID) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->cat_name.'</a> </li>' ;
+                      }
+                      ?>
+                    </ul>
+                  </div>
+                  <?php comments_template(); ?>
+                  <?php if ( function_exists( 'floating_social_bar' ) ) floating_social_bar( array( 'facebook' => true, 'twitter' => true, 'google' => true, 'linkedin' => true, 'pinterest' => true, 'static' => true ) ); ?>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      <?php endif; ?>
-      <div class="container container--narrow">
-        <div class="grid">
-          <div class="grid__item">
-            <div class="user-content">
-              <div class="single-title"><?php the_title(); ?></div>
-              <div class="single-date"><?php the_date('M d, Y'); ?></div>
-              <div class="single-content"><?php the_content(); ?></div>
-              <div class="single-categories">
-                <ul>
-                                    <?php
-                  foreach((get_the_category()) as $category) {
-                      echo '<li>' . $category->cat_name . ' </li>';
-                    // echo '<li>' . '<a href="' . get_category_link( $category->cat_ID) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->cat_name.'</a> </li>' ;
-                  }
-                  ?>
-                </ul>
-              </div>
-              <?php comments_template(); ?>
-              <?php if ( function_exists( 'floating_social_bar' ) ) floating_social_bar( array( 'facebook' => true, 'twitter' => true, 'google' => true, 'linkedin' => true, 'pinterest' => true, 'static' => true ) ); ?>
-            </div>
-          </div>
+        <div class="grid__item one-third palm--one-whole">
+          <?php get_sidebar(); ?>
         </div>
       </div>
     </div>
