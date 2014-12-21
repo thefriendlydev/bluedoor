3cb4d7f516d169f6f61fb1d7c226cff687d47537
Matt Miller <matt@milleronrails.com>
2014-12-20 23:37:47 -0500

    testing

--- yarpp-template-bluedoor.php
+++ yarpp-template-bluedoor.php
@@ -1,19 +1,16 @@
 <?php
 /*
-YARPP Template: Bluedoor
-Description: Requires a theme which supports post thumbnails
-Author: Matt Miller (www.onlineclarity.ca)
-*/ ?>
-<h3>Related Photos</h3>
+YARPP Template: Simple
+Author: mitcho (Michael Yoshitaka Erlewine)
+Description: A simple example YARPP template.
+*/
+?><h3>Related Posts</h3>
 <?php if (have_posts()):?>
 <ol>
   <?php while (have_posts()) : the_post(); ?>
-    <?php if (has_post_thumbnail()):?>
-    <li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a></li>
-    <?php endif; ?>
+  <li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
   <?php endwhile; ?>
 </ol>
-
 <?php else: ?>
-<p>No related photos.</p>
+<p>No related posts.</p>
 <?php endif; ?>
